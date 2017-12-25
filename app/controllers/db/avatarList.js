95364163ddbf59bd85b4a992c676e271a99dd698
薛涵予 <xuehanyu@sunlands.com>
2017-12-25 14:59:01 +0800

    mock欢迎页的数据

---
+++ app/controllers/db/avatarList.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "nickName": "飞飞飞",
+  "headImageUrl": "string",
+  "totle": 1236789,
+  "icons": [
+    "string",
+    "lalalla"
+  ]
+}
--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,4 +1,11 @@
-const router = require('koa-router')()
+const testTypeList = require('./db/testTypeList')
+const avatarList = require('./db/avatarList')
+// 
+exports.getTestList = async (ctx, next) => {
+  ctx.body = testTypeList
+}
+exports.getWelcomList = async (ctx, next) => {
+  ctx.body = avatarList
+}
 
-
-module.exports = router
+// getSimulateList
